ff418735602e00bf33715f8f115450aa0ce5e528
Valeriya <Lerusya.Pan@yandex.ru>
2023-09-23 20:40:21 +0700

    All my changes

---
+++ src/test/CashbackHackServiceTest.java
@@ -0,0 +1,48 @@
+package ru.netology.service;
+
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
+    CashbackHackService service = new CashbackHackService();
+
+    org.testng.annotations.Test
+    public void shouldReturn200at800() {
+        int expected = 200;
+        int actual = service.remain(800);
+
+        assertEquals(actual, expected);
+    }
+
+    org.testng.annotations.Test
+    public void shouldReturn1at999() {
+        int expected = 1;
+        int actual = service.remain(999);
+
+        assertEquals(actual, expected);
+    }
+
+    org.testng.annotations.Test
+    public void shouldReturn999at1() {
+        int expected = 999;
+        int actual = service.remain(1);
+
+        assertEquals(actual, expected);
+    }
+
+    org.testng.annotations.Test
+    public void shouldReturn0at1000() {
+        int expected = 0;
+        int actual = service.remain(1000);
+
+        assertEquals(actual, expected);
+    }
+
+    org.testng.annotations.Test
+    public void shouldReturn999at1001() {
+        int expected = 999;
+        int actual = service.remain(1001);
+
+        assertEquals(actual, expected);
+    }
+
+}
